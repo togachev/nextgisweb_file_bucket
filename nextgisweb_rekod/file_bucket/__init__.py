f5b2fcee0caa77830059bbe0573b34d5c2337d4b
Aleksandr Dezhin <me@dezhin.net>
2014-11-12 19:15:35 +0300

    Бэкап набора файлов средствами ngw

--- nextgisweb_rekod/file_bucket/__init__.py
+++ nextgisweb_rekod/file_bucket/__init__.py
@@ -1,10 +1,34 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import os.path
+from shutil import copyfileobj
 
 from nextgisweb.component import Component
+from nextgisweb.core import BackupBase
 
-from .model import Base
+from .model import Base, FileBucket
+
+
+@BackupBase.registry.register
+class FileBucketFileBackup(BackupBase):
+    identity = 'file_bucket_file'
+
+    def is_binary(self):
+        return True
+
+    def backup(self):
+        stuuid, name = self.key.split(':', 1)
+        dirname = self.comp.dirname(stuuid)
+        fname = os.path.join(dirname, name)
+        with open(fname, 'rb') as fd:
+            copyfileobj(fd, self.binfd)
+
+    def restore(self):
+        stuuid, name = self.key.split(':', 1)
+        dirname = self.comp.dirname(stuuid, makedirs=True)
+        fname = os.path.join(dirname, name)
+        with open(fname, 'wb') as fd:
+            copyfileobj(self.binfd, fd)
 
 
 @Component.registry.register
@@ -31,6 +55,15 @@ class FileBucketComponent(Component):
             os.makedirs(path)
 
         return path
+
+    def backup(self):
+        for i in super(FileBucketComponent, self).backup():
+            yield i
+
+        for file_bucket in FileBucket.query():
+            for f in file_bucket.files:
+                yield FileBucketFileBackup(
+                    self, file_bucket.stuuid + ':' + f.name)
 
     settings_info = (
         dict(key='path', desc=u"Директория для хранения файлов"),
