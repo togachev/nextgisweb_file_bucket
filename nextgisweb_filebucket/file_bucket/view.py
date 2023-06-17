4a0c51f01f468cd56bc5baf4951cab526ff6b677
Aleksandr Dezhin <me@dezhin.net>
2023-06-17 16:59:43 +0300

    Refactor page sections
    CU-85ztajzf3

--- nextgisweb_filebucket/file_bucket/view.py
+++ nextgisweb_filebucket/file_bucket/view.py
@@ -1,6 +1,7 @@
 
 from nextgisweb.lib.dynmenu import DynItem, Label, Link
 from nextgisweb.resource import Resource, Widget
+from nextgisweb.resource.view import resource_sections
 
 from .model import FileBucket
 from .util import _
@@ -27,7 +28,6 @@ class FileBucketMenu(DynItem):
 def setup_pyramid(comp, config):
     Resource.__dynmenu__.add(FileBucketMenu())
 
-    Resource.__psection__.register(
-        key='file_bucket', priority=20, title=_("File bucket"),
-        is_applicable=lambda obj: isinstance(obj, FileBucket),
-        template='nextgisweb_filebucket:file_bucket/template/section.mako')
+    @resource_sections(title=_("File bucket"), priority=20, template='nextgisweb_filebucket:file_bucket/template/section.mako')
+    def resource_section(obj):
+        return isinstance(obj, FileBucket)
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with io.open('VERSION', 'r') as fd:
     VERSION = fd.read().rstrip()
 
 requires = (
-    'nextgisweb>=4.4.0.dev9',
+    'nextgisweb>=4.4.0.dev11',
     'zipstream-new==1.1.*',
 )
 
