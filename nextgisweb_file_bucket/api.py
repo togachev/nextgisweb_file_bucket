d0eb78d946e4a6bcbb4d0c1d494160b854225e6a
Aleksandr Dezhin <me@dezhin.net>
2023-08-29 22:14:52 +0000

    Remove legacy routes

--- nextgisweb_file_bucket/api.py
+++ nextgisweb_file_bucket/api.py
@@ -3,7 +3,7 @@ from pyramid.httpexceptions import HTTPNotFound
 from pyramid.response import Response
 
 from nextgisweb.pyramid.tomb import UnsafeFileResponse
-from nextgisweb.resource import DataScope, resource_factory
+from nextgisweb.resource import DataScope
 
 from .model import FileBucket, FileBucketFile
 
@@ -12,10 +12,7 @@ def file_download(resource, request):
     request.resource_permission(DataScope.read)
 
     fname = request.matchdict["name"]
-    fobj = FileBucketFile.filter_by(
-        file_bucket_id=resource.id,
-        name=fname
-    ).one_or_none()
+    fobj = FileBucketFile.filter_by(file_bucket_id=resource.id, name=fname).one_or_none()
     if fobj is None:
         raise HTTPNotFound()
 
@@ -25,13 +22,13 @@ def file_download(resource, request):
 def export(resource, request):
     request.resource_permission(DataScope.read)
 
-    zip_stream = zipstream.ZipFile(mode='w', compression=zipstream.ZIP_DEFLATED, allowZip64=True)
+    zip_stream = zipstream.ZipFile(mode="w", compression=zipstream.ZIP_DEFLATED, allowZip64=True)
     for f in resource.files:
         zip_stream.write(f.path, arcname=f.name)
 
     return Response(
         app_iter=zip_stream,
-        content_type='application/zip',
+        content_type="application/zip",
         content_disposition='attachment; filename="%d.zip"' % resource.id,
     )
 
@@ -39,26 +36,14 @@ def export(resource, request):
 def setup_pyramid(comp, config):
     config.add_view(
         file_download,
-        route_name='resource.file_download',
-        context=FileBucket,
-        request_method='GET',
-    )
-
-    config.add_route(
-        'file_bucket.file_download',
-        r'/api/resource/{id:uint}/file_bucket/file/{name:str}',
-        factory=resource_factory
-    ).get(file_download, context=FileBucket)
-
-    config.add_view(
-        export,
-        route_name='resource.export',
+        route_name="resource.file_download",
         context=FileBucket,
         request_method="GET",
     )
 
-    config.add_route(
-        'file_bucket.export',
-        r'/api/resource/{id:uint}/file_bucket/export',
-        factory=resource_factory
-    ).get(export, context=FileBucket)
+    config.add_view(
+        export,
+        route_name="resource.export",
+        context=FileBucket,
+        request_method="GET",
+    )
