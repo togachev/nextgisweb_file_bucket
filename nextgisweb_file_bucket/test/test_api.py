8cfed226ff1b252e3f870607dcc0a542110d7ed1
Aleksandr Dezhin <me@dezhin.net>
2023-09-18 23:57:01 +0000

    Add pyproject.toml

--- nextgisweb_file_bucket/test/test_api.py
+++ nextgisweb_file_bucket/test/test_api.py
@@ -14,15 +14,18 @@ TEST_FILE3 = {"name": "white/daisy.flw", "content": "daisy".encode("utf-8")}
 def test_bucket_crud(ngw_webtest_app):
     webapp = ngw_webtest_app
 
-    resp = webapp.post("/api/component/file_upload/", [
-        ["files[]", webtest.Upload(TEST_FILE1["name"], TEST_FILE1["content"])],
-        ["files[]", webtest.Upload(TEST_FILE2["name"], TEST_FILE2["content"])],
-        ["files[]", webtest.Upload(TEST_FILE3["name"], TEST_FILE3["content"])]
-    ])
+    resp = webapp.post(
+        "/api/component/file_upload/",
+        [
+            ["files[]", webtest.Upload(TEST_FILE1["name"], TEST_FILE1["content"])],
+            ["files[]", webtest.Upload(TEST_FILE2["name"], TEST_FILE2["content"])],
+            ["files[]", webtest.Upload(TEST_FILE3["name"], TEST_FILE3["content"])],
+        ],
+    )
 
     data = {
         "resource": {"cls": "file_bucket", "display_name": "test-bucket", "parent": {"id": 0}},
-        "file_bucket": {"files": resp.json["upload_meta"]}
+        "file_bucket": {"files": resp.json["upload_meta"]},
     }
     resp = webapp.post_json("/api/resource/", data, status=201)
     bucket_id = resp.json["id"]
@@ -34,22 +37,29 @@ def test_bucket_crud(ngw_webtest_app):
     resp = webapp.get("/api/resource/%d/file/%s" % (bucket_id, TEST_FILE3["name"]), status=200)
     assert resp.body == TEST_FILE3["content"]
 
-    webapp.put_json("/api/resource/%d" % bucket_id, {
-        "file_bucket": {"files": [{"name": "iam/../bad"}]}
-    }, status=422)
+    webapp.put_json(
+        "/api/resource/%d" % bucket_id,
+        {"file_bucket": {"files": [{"name": "iam/../bad"}]}},
+        status=422,
+    )
 
-    webapp.put_json("/api/resource/%d" % bucket_id, {
-        "file_bucket": {"files": [{"name": TEST_FILE1["name"]}]}
-    }, status=200)
+    webapp.put_json(
+        "/api/resource/%d" % bucket_id,
+        {"file_bucket": {"files": [{"name": TEST_FILE1["name"]}]}},
+        status=200,
+    )
     webapp.get("/api/resource/%d/file/%s" % (bucket_id, TEST_FILE1["name"]), status=200)
     webapp.get("/api/resource/%d/file/%s" % (bucket_id, TEST_FILE2["name"]), status=404)
 
-    resp = webapp.post("/api/component/file_upload/", {
-        "file": webtest.Upload(TEST_FILE1["name"], TEST_FILE1["content"])
-    })
-    webapp.put_json("/api/resource/%d" % bucket_id, {
-        "file_bucket": {"files": resp.json["upload_meta"]}
-    }, status=200)
+    resp = webapp.post(
+        "/api/component/file_upload/",
+        {"file": webtest.Upload(TEST_FILE1["name"], TEST_FILE1["content"])},
+    )
+    webapp.put_json(
+        "/api/resource/%d" % bucket_id,
+        {"file_bucket": {"files": resp.json["upload_meta"]}},
+        status=200,
+    )
 
     webapp.delete("/api/resource/%d" % bucket_id, status=200)
     webapp.get("/api/resource/%d/file/%s" % (bucket_id, TEST_FILE3["name"]), status=404)
@@ -61,7 +71,9 @@ def test_archive(ngw_webtest_app):
 
     def make_archive(files):
         data = BytesIO()
-        with zipfile.ZipFile(data, mode="a", compression=zipfile.ZIP_DEFLATED, allowZip64=False) as archive:
+        with zipfile.ZipFile(
+            data, mode="a", compression=zipfile.ZIP_DEFLATED, allowZip64=False
+        ) as archive:
             for f in files:
                 archive.writestr(f["name"], f["content"])
         data.seek(0)
@@ -72,8 +84,12 @@ def test_archive(ngw_webtest_app):
     archive_id = make_archive([TEST_FILE1, TEST_FILE2])
 
     bucket_data = {
-        "resource": {"cls": "file_bucket", "display_name": "test-bucket-from-archive", "parent": {"id": 0}},
-        "file_bucket": {"archive": {"id": archive_id}, "files": []}
+        "resource": {
+            "cls": "file_bucket",
+            "display_name": "test-bucket-from-archive",
+            "parent": {"id": 0},
+        },
+        "file_bucket": {"archive": {"id": archive_id}, "files": []},
     }
     webapp.post_json("/api/resource/", bucket_data, status=422)
 
@@ -88,15 +104,19 @@ def test_archive(ngw_webtest_app):
     assert resp.body == TEST_FILE2["content"]
 
     archive_id = make_archive([TEST_FILE2, TEST_FILE3])
-    webapp.put_json("/api/resource/%d" % bucket_id, {
-        "file_bucket": {"archive": {"id": archive_id}}
-    }, status=200)
+    webapp.put_json(
+        "/api/resource/%d" % bucket_id,
+        {"file_bucket": {"archive": {"id": archive_id}}},
+        status=200,
+    )
     webapp.get("/api/resource/%d/file/%s" % (bucket_id, TEST_FILE1["name"]), status=404)
     webapp.get("/api/resource/%d/file/%s" % (bucket_id, TEST_FILE2["name"]), status=200)
     webapp.get("/api/resource/%d/file/%s" % (bucket_id, TEST_FILE3["name"]), status=200)
 
     resp = webapp.get("/api/resource/%d/export" % bucket_id, status=200)
-    with zipfile.ZipFile(BytesIO(resp.body), mode="r", compression=zipfile.ZIP_DEFLATED, allowZip64=False) as archive:
+    with zipfile.ZipFile(
+        BytesIO(resp.body), mode="r", compression=zipfile.ZIP_DEFLATED, allowZip64=False
+    ) as archive:
         assert archive.read(TEST_FILE2["name"]) == TEST_FILE2["content"]
         assert archive.read(TEST_FILE3["name"]) == TEST_FILE3["content"]
 
