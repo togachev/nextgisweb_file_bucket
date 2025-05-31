c052f8cfaa2d326911dbc27ff941a7bd03349e49
Aleksandr Dezhin <me@dezhin.net>
2025-05-31 16:04:26 +0000

    Deprecate DynItem
    NGW-1612

--- nextgisweb_file_bucket/view.py
+++ nextgisweb_file_bucket/view.py
@@ -1,5 +1,5 @@
 from nextgisweb.env import gettext
-from nextgisweb.lib.dynmenu import DynItem, Label, Link
+from nextgisweb.lib.dynmenu import Label, Link
 
 from nextgisweb.jsrealm import jsentry
 from nextgisweb.resource import Resource, Widget
@@ -14,23 +14,20 @@ class FileBucketWidget(Widget):
     amdmod = jsentry("@nextgisweb/file-bucket/resource-widget")
 
 
-class FileBucketMenu(DynItem):
-    def build(self, args):
-        yield Label("file_bucket", gettext("File bucket"))
-
-        if isinstance(args.obj, FileBucket):
-            if args.obj.has_export_permission(args.request.user):
-                yield Link(
-                    "file_bucket/export",
-                    gettext("Export"),
-                    lambda args: args.request.route_url("resource.export", id=args.obj.id),
-                )
-
-
 @resource_sections("@nextgisweb/file-bucket/resource-section")
 def resource_section(obj, **kwargs):
     return isinstance(obj, FileBucket)
 
 
 def setup_pyramid(comp, config):
-    Resource.__dynmenu__.add(FileBucketMenu())
+    @Resource.__dynmenu__.add
+    def _resource_dynmenu(args):
+        yield Label("file_bucket", gettext("File bucket"))
+
+        if isinstance(args.obj, FileBucket):
+            if args.obj.has_export_permission(args.request.user):
+                yield Link(
+                    "file_bucket/export",
+                    label=gettext("Export"),
+                    url=lambda args: args.request.route_url("resource.export", id=args.obj.id),
+                )
